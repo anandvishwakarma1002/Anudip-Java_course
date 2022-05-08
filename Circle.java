ac7727646c2c7febb723d31f12405cdf60db362a
Anand Vishwakarma <52778294+anandvishwakarma1002@users.noreply.github.com>
2022-05-09 03:54:33 +0530

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -3,18 +3,19 @@ public class Circle
 {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.print("Enter the radius: ");
+        
+        System.out.print("Enter The Radius: ");
         /*We are storing the entered radius in double
          * because a user can enter radius in decimals
          */
-        double radius = sc.nextDouble();
+        double Radius = sc.nextDouble();
 
-        //Area = PI*radius*radius
-        double area = Math.PI * (radius * radius);
-        System.out.println("The area of circle is: " + area);
+        //Area = PI*Radius*Radius
+        double Area = Math.PI * (Radius * Radius);
+        System.out.println("The Area Of Circle Is: " + Area);
 
-        //Circumference = 2*PI*radius
-        double circumference= Math.PI * 2*radius;
-        System.out.println( "The circumference of the circle is: "+circumference) ;
+        //Circumference = 2*PI*Radius
+        double Circumference= Math.PI * 2*Radius;
+        System.out.println( "The Circumference Of Circle Is: "+Circumference) ;
     }
 }
